353e579177d1ecee4bce482a48c56ab4d349061a
Andreas Rozek <A.Rozek@gmx.de>
2021-06-15 07:54:22 +0200

    add TypeScript type declaration file

---
+++ agnostic-draggable.d.ts
@@ -0,0 +1,9 @@
+  type EventHandlers = { [Name:string]:Function }
+
+  declare const ConstructorSet:{
+    Draggable: new (draggableElement:Element, Options:any, Handlers:EventHandlers) => any
+    Droppable: new (droppableElement:Element, Options:any, Handlers:EventHandlers) => any
+    Sortable:  new (sortableElement:Element,  Options:any, Handlers:EventHandlers) => any
+  }
+  export default ConstructorSet
+
